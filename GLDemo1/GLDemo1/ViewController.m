a8436a66428e7283572ecca8d41775fc6524a792
伍陶陶 <a@gotoutoutekiMacBook-Pro.local>
2017-04-23 21:16:04 +0800

    GL

--- GLDemo1/GLDemo1/ViewController.m
+++ GLDemo1/GLDemo1/ViewController.m
@@ -39,13 +39,10 @@
 -(void)uploadVertexArray{
     GLfloat squareVertexData[] = {
         
-        0.5, -0.5, 0.0f,  1.0f, 0.0f,
-        0.5, 0.5, -0.0f,  1.0f, 1.0f,
-        -0.5, 0.5, 0.0f,  0.0f, 1.0f,
+        0.5, -0.5, 0.2f,
+        0.5, 0.1, -0.0f,
+        -0.5, 0.5, 0.3f,
         
-        0.5, -0.5, 0.0f,  1.0f, 0.0f,
-        -0.5, 0.5, 0.0f,  0.0f, 1.0f,
-        -0.5, -0.5, 0.0f, 0.0f, 0.0f,
         
     };
     
@@ -56,14 +53,13 @@
     // GL_STATIC_DRAW  数据几乎不会改变
     // GL_DYNAMIC_DRAW  数据会被改变很多
     // GL_STREAM_DRAW  数据每次绘制时都会改变
-    
+    // 开启渲染使用缓存数据
     glEnableVertexAttribArray(GLKVertexAttribPosition);//ding dain shu ju huan cun
+ // 需要访问缓存数据的类型和所有需要访问的数据的内存偏移值
     glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 5, (GLfloat *)NULL + 0);
     glEnableVertexAttribArray(GLKVertexAttribTexCoord0);
     glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 5, (GLfloat *)NULL + 3);
     
-    
-
 
 }
 
@@ -71,14 +67,15 @@
 
 -(void)uploadTexture{
     
-    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"for_test" ofType:@"jpg"];
-    NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:@(1),GLKTextureLoaderOriginBottomLeft, nil];
-    
-    GLKTextureInfo *textureInfo = [GLKTextureLoader textureWithContentsOfFile:filePath options:options error:nil];
-    
+//    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"for_test" ofType:@"jpg"];
+//    NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:@(1),GLKTextureLoaderOriginBottomLeft, nil];
+//    
+//    GLKTextureInfo *textureInfo = [GLKTextureLoader textureWithContentsOfFile:filePath options:options error:nil];
+    //
+    //初始化一个默认的GPU 着色器程序
     self.mEffect = [[GLKBaseEffect alloc]init];
     self.mEffect.texture2d0.enabled = GL_TRUE;
-    self.mEffect.texture2d0.name = textureInfo.name;
+//    self.mEffect.texture2d0.name = textureInfo.name;
     
     
 //    glCreateShader(GL_VERTEX_SHADER);
@@ -91,7 +88,7 @@
     
     //启动着色器
     [self.mEffect prepareToDraw];
-    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glDrawArrays(GL_LINES, 0, 3);
 }
 
 
