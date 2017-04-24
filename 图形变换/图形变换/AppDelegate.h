a3ff4da37a740844fa9edcf8b72d51a3562674b3
wutaotao <wutaotao@wutaotaodeMac-mini.local>
2017-04-24 08:55:01 +0800

    triangle

---
+++ 图形变换/图形变换/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  图形变换
+//
+//  Created by wutaotao on 2017/4/20.
+//  Copyright © 2017年 wutaotao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 图形变换/图形变换/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  图形变换
+//
+//  Created by wutaotao on 2017/4/20.
+//  Copyright © 2017年 wutaotao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
