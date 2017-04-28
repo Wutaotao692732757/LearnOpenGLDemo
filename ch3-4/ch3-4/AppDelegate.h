04500c227aa3cd67ecaa5fc6cb4e373684e206fd
Wutaotao692732757 <692732757@qq.com>
2017-04-28 08:54:00 +0800

    ch3-4

---
+++ ch3-4/ch3-4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ch3-4
+//
+//  Created by wutaotao on 2017/4/28.
+//  Copyright © 2017年 wutaotao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
